8233e9ec3cafa2c1dae4be66450ef4190b0e975c
Don <donp@donp.org>
2022-06-20 11:30:41 -0700

    deploy

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@
 all:
 	./node_modules/.bin/truffle build
 
+deploy:
+	./node_modules/.bin/truffle deploy
+
 format:
 	npx prettier --write contracts/**/*.sol
 
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const Snipe = artifacts.require("Snipe");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Snipe);
 };
