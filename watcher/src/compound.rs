0425b71b5db995dae3600bc1215951aefaac7dd9
Don <donp@donp.org>
2022-06-20 14:32:45 -0700

    missing files

---
+++ watcher/src/compound.rs
@@ -0,0 +1,1 @@
+pub static ACCOUNTS_URL: &str = "https://api.compound.finance/api/v2/account";
---
+++ watcher/src/update.rs
@@ -0,0 +1,19 @@
+use crate::compound::*;
+use hyper::Client;
+use hyper_tls::HttpsConnector;
+
+#[tokio::main]
+pub async fn update() -> Result<hyper::Response<hyper::Body>, Box<dyn std::error::Error + Send + Sync>> {
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
+    // Parse an `http::Uri`...
+    let uri = ACCOUNTS_URL.parse()?;
+    println!("{}", uri);
+
+    // Await the response...
+    let resp = client.get(uri).await?;
+    
+    
+    Ok(resp)
+}
